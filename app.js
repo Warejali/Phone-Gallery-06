78ea6c29fc48ebc7b9dc4bcfd74b2f8ddd1ae0d2
Warejali <waraj.com@gmail.com>
2022-03-01 01:10:26 +0600

    few change

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ const loadData = () => {
 
 const displayData = data => {
     const items = document.getElementById('items')
-    items.innerHTML = ""
+    items.innerHTML = "";
+    const fullItem = document.getElementById('details');
+    fullItem.innerHTML = "";
     data.forEach(phone => {
-        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
       <div class="card h-100">
          <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <h5 class="card-title">${phone.brand}</h5>
-               <p class="card-text"></p>
-               <button onclick="" id="details-btn" class="btn btn-secondary">Details</button>   
+               <h5 class="card-title">${phone.phone_name}</h5>
+               <h6 class="card-title">${phone.brand}</h6>
+               <button onclick="loadDetials('${phone.slug}')" id="details-btn" class="btn btn-secondary">Details</button>   
             </div>
       </div>
       `
@@ -29,8 +30,8 @@ const displayData = data => {
     })
 }
 
-const loadDetials = idMeal =>{
-    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+const loadDetials = slug =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayDetails(data.data))
@@ -42,12 +43,19 @@ const displayDetails = phone =>{
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img src="${phone}" class="card-img-top" alt="">
-    <div class="card-body">
-      <h5 class="card-title">${phone}</h5>
-      <p class="card-text">${phone}</p>
-      <p class="card-text"><small class="text-muted">Category:${phone}</small></p>
+    
+    <div class="row g-0 py-5">
+    <div class="col-md-3 text-center">
+      <img src="${phone.image}" class="img-fluid rounded-start" alt="" style="height:200px">
     </div>
+    <div class="col-md-9">
+      <div class="card-body">
+        <h5 class="card-title">${phone.name}</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
     `;
     fullItem.appendChild(div)
 }
