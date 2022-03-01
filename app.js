81dc99a65e53c5dda97c1f11eb715609aaa076de
Warejali <waraj.com@gmail.com>
2022-03-01 15:50:52 +0600

    Almost done without revision

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const loadData = () => {
     .then(data => displayData(data.data))
 }
 
+/* Display all search result */
 const displayData = data => {
     const items = document.getElementById('items')
     items.innerHTML = "";
@@ -37,6 +38,7 @@ const loadDetials = slug =>{
     .then(data => displayDetails(data.data))
 }
 
+/* Display phone details */
 const displayDetails = phone =>{
     const fullItem = document.getElementById('details');
     fullItem.innerHTML = "";
@@ -44,18 +46,76 @@ const displayDetails = phone =>{
     div.classList.add('card');
     div.innerHTML = `
     
-    <div class="row g-0 py-5">
-    <div class="col-md-3 text-center">
-      <img src="${phone.image}" class="img-fluid rounded-start" alt="" style="height:200px">
+    <div class="row g-3 p-3">
+        <div class="col-md-4 align-item-center justify-content-center">
+            <img src="${phone.image}" class="w-100" alt="">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="card-title">${phone.name}</h2>
+                <h6 class="card-title">${phone.releaseDate || 'Unknown Release Date'}</h6>
+                <p class="card-text"><small class="text-muted"></small></p>
+
+                <h5>Main Features:</h5>
+                <table class="table table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th class ="table-primary">Storage:</th>
+                            <th class="table-danger">DisplaySize:</th>
+                            <th class="table-secondary">ChipSet:</th>
+                            <th class="table-warning">Memory</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="table-info">${phone.mainFeatures.storage} </td>
+                            <td class="table-warning">${phone.mainFeatures.displaySize} </td>
+                            <td class="table-success">${phone.mainFeatures.chipSet} </td>
+                            <td class="table-danger">${phone.mainFeatures.memory} </td>
+                        </tr>
+                    </tbody>
+                </table>    
+
+            <table class="table table-bordered text-center">
+                <thead>
+                    <h5>Sensors:</h5>
+                    <tr class="table-info">
+                        <td>${phone.mainFeatures.sensors[0]}:</td>
+                        <td>${phone.mainFeatures.sensors[1]}</td>
+                        <td>${phone.mainFeatures.sensors[2]}</td>
+                        <td>${phone.mainFeatures.sensors[3]}</td>
+                        <td>${phone.mainFeatures.sensors[4]}</td>
+                        <td>${phone.mainFeatures.sensors[5]}</td>
+                    </tr>
+                </thead>
+            </table>  
+
+    <h5>Others:</h5>
+    <table class="table table-bordered text-center">
+        <thead>
+            <tr>
+                <th class="table-danger">WLAN:</th>
+                <th class="table-success">Bluetooth:</th>
+                <th class="table-warning">GPS:</th>
+                <th class="table-info">NFC</th>
+                <th class="table-danger">Radio</th>
+                <th class="table-info">USB</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="table-secondary">${phone.others.WLAN} </td>
+                <td class="table-danger">${phone.others.Bluetooth} </td>
+                <td class="table-info">${phone.others.GPS}</td>
+                <td class="table-warning">${phone.others.NFC}</td>
+                <td class="table-primary">${phone.others.Radio}</td>
+                <td class="table-secondary">${phone.others.USB}</td>
+            </tr>
+        </tbody>
+    </table>
+            </div>
+        </div>
     </div>
-    <div class="col-md-9">
-      <div class="card-body">
-        <h5 class="card-title">${phone.name}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
     `;
     fullItem.appendChild(div)
 }
